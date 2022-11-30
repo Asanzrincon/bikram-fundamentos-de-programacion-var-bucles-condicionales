64969670a7bd6a1c11b37b84f69dce4be418a37b
BlinkPayTeam <alejandrosanzrinconcorreo@gmail.com>
2022-11-30 18:37:50 +0100

    bikramvarbuclescondicionales

--- index.js
+++ index.js
@@ -5,118 +5,122 @@ let variableSinValor;
 
 // 2.- 
 
-let booleano1= true;
-let booleano2= false;
+let booleano1 = true;
+let booleano2 = false;
 
-// 3.-
+// 3.- 
 
-const PI= 3.14;
+const PI = 3.14;
 
 // 4.-
 
-const TAU= 2*PI;
+const TAU = 2*PI;
 
 //BOOLEANOS
 // 5.-
 
-let booleanoAnd= booleano1 && booleano2;
+let booleanoAnd = booleano1 && booleano2;
 
 // 6.-
 
-let booleanoNot= !booleano1;
-
+let booleanoNot =  !booleano1;
+ 
 // 7.-
 
-let booleanoMix0= (booleano1||booleano2)&&(booleano1||(!booleano1&&!booleano2));
+let booleanoMix0 = (booleano1||booleano2)&&((booleano1||(!booleano1&&!booleano2)));
 
-//OPRADORES
-// 8.-
+//OPERADORES
+// 8.- 
 
-let incrementarDesp= 2;
-let resultadoDesp= incrementarDesp++;
+let  incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
 
-// 9.-
+// 9.- 
 
-let icrementarAntes = 2;
-let resultadoAntes= ++ incrementarAntes;
+let incrementarAntes = 2;
+resultadoAntes = ++incrementarAntes;
 
-//BUCLES
-// 10.-
+//Bucles
+// 10.- 
 
-let contarHasta10_2=0;
-for(let i=0; i <10; i++);
-{contarHasta10_2++;
+let contarHasta10_2 = 0;
+for(let i=0; i <10;i++){
+    contarHasta10_2++;
 }
 
-// 11.-
+// 11.- 
 
-let postI=0;
-let postJ=0;
 
-for(let i=0; i <11; i++){
-    postI += postJ++;
+let postI = 0;
+let postJ = 0;
+
+for(let i= 0; i <11; i++){
+    postI+= postJ++;  
 }
 
-// 12.-
+// 12.- 
 
-let sumaPares=0;
-for (let i=0; i <10; i++){
-   if (i%2===0) {
-       sumaPares+=i;
-   } else{
-   }
-}
+ let sumaPares = 0;
+ 
+ for (let i=0; i <10; i++){
+    if (i%2===0) {
+        sumaPares+=i;
+    } else{
+    }
+ }
 
-//VARIABLES
-// 13.-
+ //VARIABLES
+ // 13.-
 
-let variableValorNumerico=2;
+let variableValorNumerico = 2;
 
 // 14.-
 
-const MiNombre="Alejandro";
+const MiNombre = "Alejandro";
 
-// 15.-
+// 15.- Crear variable tipo const de nombre MiNumeroFav declarada con valor numérico
 
-const MiNumeroFav=2;
+const MiNumeroFav = 2;
 
 //BOOLEANOS
-// 16.-
+// 16.- 
 
-let booleano0r=booleano1 && booleano2;
+let booleanoOr = booleano1||booleano2;
 
 // 17.-
 
 let booleanoMix1 = (booleano1 && (TAU/2 === PI)|| (variableValorNumerico >=MiNumeroFav));
 
-// 18.-
 
-let seisNoEsNueve=6!==9;
+// 18.- 
+
+let seisNoEsNueve = 6!==9;
 
 // 19.-
 
-let booleanoMix2= (variableValorNumerico > 0 || (MiNumeroFav*TAU));
+let booleanoMix2=(variableValorNumerico > 0||(MiNumeroFav * TAU)<(MiNumeroFav * TAU));
 
-// 20.-
+// 20.- 
 
 let valorSuma=MiNumeroFav+variableValorNumerico;
 
-// 21.-
+// 21.- Crear variable valorResta cuyo valor sea la resta de MiNumeroFav y variableValorNumerico
 
-let valorResta= MiNumeroFav-variableValorNumerico;
+let valorResta=MiNumeroFav-variableValorNumerico;
 
 // 22.-
 
-let valorMultiplicacion=MiNumeroFav*variableSinValor;
+let valorMultiplicacion= MiNumeroFav* variableValorNumerico;
 
 // 23.-
 
 let valorDivision=MiNumeroFav/3;
 
-// 24.-
+// 24.- 
 
 let contarHasta10 =0;
 let i=0;
+
 while (i < 10) {
     i++;
     contarHasta10 += i;
@@ -125,7 +129,7 @@ while (i < 10) {
     }
 }
 
-// 25.-
+// 25.- 
 
 let preI = 0;
 let preJ =0;
@@ -135,8 +139,11 @@ for(let i=0; i<11; i++){
 
 
 // 26.-
+
 let sumaImpares =0;
+
 for(let i = 0; i < 10; i++){
+
     if(i%2 !== 0){
         sumaImpares +=i;
     }
